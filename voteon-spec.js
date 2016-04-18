d33dce3c8ed956f52bbc9f2d5c5207a822069223
David Edwards <david.edwards@scottlogic.co.uk>
2016-04-18 17:24:06 +0100

    more

--- voteon-spec.js
+++ voteon-spec.js
@@ -1,17 +1,21 @@
 // Create your tests here against https://votingapp.azurewebsites.net/Poll/#/94df22b2-ecf5-480c-b558-890c93798bc7/Vote/
 
 describe('VoteOn demo tests', function() {
-  it('should have the right title', function() {
-    browser.get('https://votingapp.azurewebsites.net/Poll/#/94df22b2-ecf5-480c-b558-890c93798bc7/Vote/');
-
-	expect(browser.getTitle()).toEqual('Vote On');
+	
+	beforeEach(function() {
+		browser.get('https://votingapp.azurewebsites.net/Poll/#/94df22b2-ecf5-480c-b558-890c93798bc7/Vote/');
+		expect(browser.getTitle()).toEqual('Vote On');
+	});
+	
+	
+  it('requires a username to vote', function() {
 	
 	var input = element.all(by.id('voter-name-input')).first();
 	input.clear();
 	element(by.id('vote-button')).click();
 	expect(element.all(by.id('voter-name-required-validation-message')).first().getText()).toEqual('You must supply a name');
 	
-  });
+	});
   
   
 });
